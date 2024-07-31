2cd999cbbc90fc7f170318967d8f18981cc640f0
sahand mahdilu <sahand.mahdilu29@gmail.com>
2024-07-31 22:41:56 +0330

    logic : component created and data added using  destructuring props

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,25 @@ import './App.css'
 import ProductCart from './Product cart/ProductCart'
 
 function App() {
+
+  const products=[
+    {id:1, name:"Asus",image:"images/1.jpg", price:1500 ,count: 11},
+    {id:2, name:"HP",image:"images/2.jpg", price:2500 ,count: 19},
+    {id:3, name:"Dell",image:"images/3.jpg", price:2000 ,count: 12},
+  ]
+  
+  const arr =[1,2,3,4]
+
+  const newP={...products}
+  console.log(newP)
+
   
 
   return <div className='container'>
-<ProductCart/>
-  <ProductCart/>
-  <ProductCart/>
+<ProductCart {...products[0]}/>
+<ProductCart {...products[1]}/>
+<ProductCart {...products[2]}/>
+  
   
   </div>
   
--- src/Product cart/ProductCart.jsx
+++ src/Product cart/ProductCart.jsx
@@ -1,13 +1,31 @@
 import './productCart.css'
 
-export default function ProductCart() {
-  return (
-    <div className='product'>
-      <h3 className='title'>Product cart</h3>
-      <img className='img' src="./public/images/1.jpg" alt="" />
-      <p className='productName'>HP</p>
-      <p className='price'>$2000</p>
-      <p>Lorem fdgd sdfsdsdfsd sdadasd sadsad sadasdsa asd sadsadas sadsad sadsad</p>
-    </div>
-  )
+export default function ProductCart(props) {
+
+
+let {name ,image="images/3.jpg" , price, count}= props // setting default value
+// console.log(props)
+
+    return (
+
+        <div className='product'>
+          <h3 className='title'>Product cart</h3>
+          <img className='img' src={image} alt="" />
+          <p className='productName'>{name}</p>
+          <p className='price'>${price}</p>
+          <p className='discription'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic nihil adipisci veritatis! Quae,  </p>
+          <button>Add to cart</button>
+        </div>
+      )
+
+
+
 }
+
+// not recommended
+// setting default data
+
+
+// ProductCart.defaultProps={
+//     image:"images/3.jpg"
+// }
